e5da55f4b1f56e9403e9a0ff18b3af76a9a64b75
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-09-16 16:04:57 +0300

    add splash view body

---
+++ lib/features/splash_view/presentation/views/splash_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/splash_view/presentation/views/widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  static const String routeName = 'splash';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SplashViewBody());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruits_hub/core/helper_function/on_generate_route.dart';
 import 'package:fruits_hub/generated/l10n.dart';
-import 'package:fruits_hub/view/splash_view/splash_view.dart';
+import 'package:fruits_hub/features/splash_view/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const FruitsHub());
@@ -20,7 +21,8 @@ class FruitsHub extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      home: SplashView(),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
     );
   }
 }
