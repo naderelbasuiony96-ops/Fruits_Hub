1fe1015e60ad24f53b129b85c641cbf652b888ab
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-09-16 16:40:33 +0300

    splash view done

--- lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [SvgPicture.asset(Assets.imagesPlant)],
+          ),
+          SvgPicture.asset(Assets.imagesLogo, width: 200),
+          SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/helper_function/on_generate_route.dart';
-import 'package:fruits_hub/generated/l10n.dart';
 import 'package:fruits_hub/features/splash_view/presentation/views/splash_view.dart';
 
 void main() {
@@ -10,17 +8,10 @@ void main() {
 
 class FruitsHub extends StatelessWidget {
   const FruitsHub({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
     );
