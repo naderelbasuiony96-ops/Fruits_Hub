5600ed2a684ec7961262a4a497abb6bd512035e3
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-09-19 12:48:39 +0300

    add login  view

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/auth/presentation/view/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const String routeName = 'login_view';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Login View')));
+    return Scaffold(body: LoginViewBody());
   }
 }
