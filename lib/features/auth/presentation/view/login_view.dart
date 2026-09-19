8cfde3f012bb852c39951c5d66a5b068cd537462
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-09-19 14:17:17 +0300

    DONE CUSTOM TEXT FORM FIELD

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruits_hub/features/auth/presentation/view/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,6 +8,9 @@ class LoginView extends StatelessWidget {
   static const String routeName = 'login_view';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LoginViewBody());
+    return Scaffold(
+      appBar: buildAppBar(context, title: 'تسجيل دخول'),
+      body: LoginViewBody(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/helper_function/on_generate_route.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singlton.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/features/splash_view/presentation/views/splash_view.dart';
 import 'package:fruits_hub/generated/l10n.dart';
 
@@ -24,7 +25,12 @@ class FruitsHub extends StatelessWidget {
       ],
       supportedLocales: S.delegate.supportedLocales,
       locale: const Locale('ar'),
-      theme: ThemeData(fontFamily: 'Cairo'),
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(backgroundColor: Colors.white, elevation: 0),
+      ),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
